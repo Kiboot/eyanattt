fb79956cc6dd595a6fee070b5d60a41d330f3773
Kiboot <kiboot@users.noreply.github.com>
2019-05-22 19:10:30 +0800

    Initial commit

---
+++ app/src/main/java/eyana/uic/edu/ph/aljunstictactoeai/MainActivity.java
@@ -0,0 +1,13 @@
+package eyana.uic.edu.ph.aljunstictactoeai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
